6a7dd8cadf1650eb73ad64e7841a5b5413da6822
royerjmasache <josethxm@gmail.com>
2019-04-18 13:45:02 -0500

    Taller hecho en clase

--- Ejercicios/Problema1/src/problema1/Problema1.java
+++ Ejercicios/Problema1/src/problema1/Problema1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package valormensual;
+package problema1;
 
 import java.util.Scanner;
 
@@ -11,7 +11,7 @@ import java.util.Scanner;
  *
  * @author royerjmasache
  */
-public class ValorMensual {
+public class Problema1 {
 
     /**
      * @param args the command line arguments
@@ -19,15 +19,18 @@ public class ValorMensual {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         double sueldot, sueldo, numhoras, costo, descuento;
+        // Ingreso de datos
         System.out.println("Ingrese el número de horas");
         numhoras = entrada.nextDouble();
         System.out.println("Ingrese el costo por hora oficial");
         costo = entrada.nextDouble();
+        // Operación
         sueldot = numhoras * costo;
         descuento = sueldot * 0.1;
         sueldo =  sueldot - descuento;
-        System.out.printf("El descuento realizado al Seguro Social es de: %.2f\n"
+        // Presentación de datos
+        System.out.printf("El descuento por el Seguro Social es de:%.2f\n"
                 + "El sueldo mensual del trabajador es de: %.2f\n", descuento,
                 sueldo);
-    }    
+    }
 }
